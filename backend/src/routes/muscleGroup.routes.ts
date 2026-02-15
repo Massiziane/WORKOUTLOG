4fbe864c519b3fa97603c8e388aca47bbd5d82b0
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-02-15 15:04:13 -0500

    data insertion for testing and slight backend correction

--- backend/src/routes/muscleGroup.routes.ts
+++ backend/src/routes/muscleGroup.routes.ts
@@ -1,9 +1,29 @@
 import { Router } from 'express';
-import { getMuscleGroups } from '../controllers/muscleGroup.controller';
+import {
+    getAllMuscleGroups,
+    getMuscleGroupById,
+    createMuscleGroup,
+    updateMuscleGroup,
+    deleteMuscleGroup
+} from '../controllers/muscleGroup.controller';
+
 
 const router = Router();
 
 // GET muscle groups
-router.get("/", getMuscleGroups);
+router.get("/", getAllMuscleGroups);
+
+// GET muscle group by ID
+router.get("/:id", getMuscleGroupById);
+
+// POST create a new muscle group
+router.post("/", createMuscleGroup);
+
+// PUT update muscle group by ID
+router.put("/:id", updateMuscleGroup);
+
+// DELETE muscle group by ID
+router.delete("/:id", deleteMuscleGroup);
+
 
 export default router;
