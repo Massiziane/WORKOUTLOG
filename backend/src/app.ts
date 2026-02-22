7a8849de15aaff96a6da091c739762afa74b5b43
Mohamed Massinissa, Ziane <e2185817@cmaisonneuve.qc.ca>
2026-02-22 15:25:28 -0500

    redirection deployment 404 error fix attempt 5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,8 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://workoutlog-c5r8.onrender.com"
+    "https://workoutlog-c5r8.onrender.com",
+    "https://workoutlog-git-cleaning-massis-projects-e1fe0f08.vercel.app"
   ],
   credentials: true
 }));
